2969c97a05b13dc127dbe82d407a64db6caa94b0
huang231 <huang231@indiana.edu>
2018-04-26 20:49:41 -0400

    222

---
+++ S308.FinalProject.Group1/FitnessClub/FeaturePrice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessClub
+{
+    public class FeaturePrice
+    {
+        //set class properties
+        public string FeatureType { get; set; }
+        public decimal Price { get; set; }
+       
+
+
+        //set class constructor
+        public FeaturePrice()
+        {
+            FeatureType = "";
+            Price = 0;
+        }
+
+        //add class method: add membership type price information
+        public FeaturePrice(string featureType, decimal price)
+        {
+            FeatureType = featureType;
+            Price = price;
+        }
+    }
+}
